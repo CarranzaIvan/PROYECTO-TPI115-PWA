28f02a6d47ae867334fdc2d3d60701f37d9205a1
CarranzaIvan <113496779+CarranzaIvan@users.noreply.github.com>
2023-11-16 23:03:32 -0600

    PWA
    El siguiente codigo convierte en PWA todo.

---
+++ serviceWorker.js
@@ -0,0 +1,21 @@
+const staticMinerva = "minerva-connect-site-v1"
+const assets = [
+    "/",
+    "/index.html"
+  ]
+  
+  self.addEventListener("install", installEvent => {
+    installEvent.waitUntil(
+      caches.open(staticMinerva).then(cache => {
+        cache.addAll(assets)
+      })
+    )
+  })
+  
+  self.addEventListener("fetch", fetchEvent => {
+      fetchEvent.respondWith(
+        caches.match(fetchEvent.request).then(res => {
+          return res || fetch(fetchEvent.request)
+        })
+      )
+    })
