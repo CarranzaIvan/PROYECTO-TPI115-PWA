e3be1430feba3af6e5e0512f9d377f214fba5c04
bel-anaya <72322567+M-bel@users.noreply.github.com>
2023-11-28 22:59:25 -0600

    notificacion

---
+++ Notificacion/indexNot.js
@@ -0,0 +1,92 @@
+const contenedorBotones = document.getElementById('contenedor-botones');
+const contenedorToast = document.getElementById('contenedor-toast');
+
+// Event listener para detectar click en los botones
+contenedorBotones.addEventListener('click', (e) => {
+	e.preventDefault();
+	const tipo = e.target.dataset.tipo;
+	agregarToast({ tipo: 'info', titulo: 'Info', descripcion: 'Esta es una notificación de información.' });
+	
+});
+
+// Event listener para detectar click en los toasts
+contenedorToast.addEventListener('click', (e) => {
+	const toastId = e.target.closest('div.toast').id;
+
+	if (e.target.closest('button.btn-cerrar')) {
+		cerrarToast(toastId);
+	}
+});
+
+// Función para cerrar el toast
+const cerrarToast = (id) => {
+	document.getElementById(id)?.classList.add('cerrando');
+};
+
+// Función para agregar la clase de cerrando al toast.
+const agregarToast = ({ tipo, titulo, descripcion, autoCierre }) => {
+	// Crear el nuevo toast
+	const nuevoToast = document.createElement('div');
+
+	// Agregar clases correspondientes
+	nuevoToast.classList.add('toast');
+	nuevoToast.classList.add(tipo);
+	if (autoCierre) nuevoToast.classList.add('autoCierre');
+
+	// Agregar id del toast
+	const numeroAlAzar = Math.floor(Math.random() * 100);
+	const fecha = Date.now();
+	const toastId = fecha + numeroAlAzar;
+	nuevoToast.id = toastId;
+
+	// Iconos
+	const iconos = {
+		info: `<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
+								<path
+									d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"
+								/>
+							</svg>`
+	};
+
+	// Plantilla del toast
+	const toast = `
+		<div class="contenido">
+			<div class="icono">
+				${iconos[tipo]}
+			</div>
+			<div class="texto">
+				<p class="titulo">${titulo}</p>
+				<p class="descripcion">${descripcion}</p>
+			</div>
+		</div>
+		<button class="btn-cerrar">
+			<div class="icono">
+				<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
+					<path
+						d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"
+					/>
+				</svg>
+			</div>
+		</button>
+	`;
+		// Agregar la plantilla al nuevo toast
+		nuevoToast.innerHTML = toast;
+
+		// Agregamos el nuevo toast al contenedor
+		contenedorToast.appendChild(nuevoToast);
+	
+		// Función para menajera el cierre del toast
+		const handleAnimacionCierre = (e) => {
+			if (e.animationName === 'cierre') {
+				nuevoToast.removeEventListener('animationend', handleAnimacionCierre);
+				nuevoToast.remove();
+			}
+		};
+	
+		if (autoCierre) {
+			setTimeout(() => cerrarToast(toastId), 5000);
+		}
+	
+		// Agregamos event listener para detectar cuando termine la animación
+		nuevoToast.addEventListener('animationend', handleAnimacionCierre);
+};
